e2b825f4f662bf20383341246842d8387039a7b1
rfaulk <bobfaulk@live.com>
2014-04-20 15:11:20 -0500

    added Foo

---
+++ foo.java
@@ -0,0 +1,16 @@
+public class foo{
+
+    public static void main(String[] args){
+        System.out.println(add(2,3));
+            System.out.println(minus(5,3));
+    }
+
+    public static int add(int x, int y){
+        return x + y;
+    }
+
+    public static int minus(int x, int y){
+        return x - y;
+    }
+
+}
