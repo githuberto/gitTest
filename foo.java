0c40d55008f95d1082ef161f84feecd27bd98f06
rfaulk <bobfaulk@live.com>
2014-04-20 15:15:56 -0500

    breaking foo

--- foo.java
+++ foo.java
@@ -2,11 +2,11 @@ public class foo{
 
     public static void main(String[] args){
         System.out.println(add(2,3));
-            System.out.println(minus(5,3));
+        System.out.println(minus(5,3));
     }
 
     public static int add(int x, int y){
-        return x + y;
+        return x + 2 * y;
     }
 
     public static int minus(int x, int y){
